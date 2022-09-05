d6a7bddd8b26c7f0ff281d8416f3dad2f6b17d49
Harsh Nandwani <hn.on.fire@gmail.com>
2022-09-05 13:34:01 +0530

    Update closePanel.py

--- src/marsrovermanipal_c/scripts/closePanel.py
+++ src/marsrovermanipal_c/scripts/closePanel.py
@@ -29,7 +29,7 @@ display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path
 lid_Store_position = rospy.getParam('lidStorage')
 # arucoID = 12
 move_group.go([0,-(pi/2), 0, -(pi/2), 0, 0])
-Inspec_panel = rospy.get_param('tag12')[0]#[0.33565261545991887, -0.2745230369010141, 0.2059755184403631]
+Inspec_panel = rospy.get_param('tag12')#[0.33565261545991887, -0.2745230369010141, 0.2059755184403631]
 GoToLid(lid_Store_position, move_group)
 gripperPos("semi_close")
 rospy.sleep(4)
